bbecd34d33bce262ab36e1709c4faf9d975101bf
AnyTime <secstar1223@gmail.com>
2023-04-24 15:45:08 -0400

    complete Login & add Form extra

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,13 +1,15 @@
 import Header from "../components/Header"
+import Login from "../components/Login";
 export default function LoginPage(){
     return(
         <>
             <Header
-                heading="Login to your account"
+                heading={"Login to your account"}
                 paragraph={"Don't have an account yet"}
                 linkName={"SignUp"}
                 linkUrl="/signup"
             />
+            <Login/>
         </>
     )
 }
