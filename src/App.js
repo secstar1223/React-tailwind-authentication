523c5fee08210a4906a3d8728ab762fc6e607e63
AnyTime <secstar1223@gmail.com>
2023-04-25 02:06:17 -0400

    final commit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LoginPage from './pages/Login'; 
+import SignupPage from './pages/Signup';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <BrowserRouter>
           <Routes>
               <Route path="/" element={<LoginPage/>} />
+              <Route path="/signup" element={<SignupPage/>} />
           </Routes>
         </BrowserRouter>
       </div>
--- src/components/Login.js
+++ src/components/Login.js
@@ -22,7 +22,19 @@ export default function Login(){
 
     //Handle Login API Integration here
     const authenticateUser = () =>{
-
+        const endpoint=`https://api.loginradius.com/identity/v2/auth/login?apikey=${apiKey}`;
+        fetch(endpoint,
+            {
+            method:'POST',
+            headers: {
+            'Content-Type': 'application/json'
+            },
+            body:JSON.stringify(loginFields)
+            }).then(response=>response.json())
+            .then(data=>{
+               //API Success from LoginRadius Login API
+            })
+            .catch(error=>console.log(error))
     }
 
     return(
